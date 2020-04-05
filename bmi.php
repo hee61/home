0e421aa389e1389c36071c791c1543ef98049f1f
hee61 <31370094+hee61@users.noreply.github.com>
2020-04-04 21:58:51 -0500

    Add files via upload

--- bmi.php
+++ bmi.php
@@ -7,40 +7,56 @@
   <link rel="stylesheet" href="">
 </head>
 	<body>
-	
 
 	<?php 
 	$bmi_button = $_POST['bmiButton'];
 	$feet = $_POST['feet'];
-		
 	$inches = $_POST['inches'];
 	$weight_lbs = $_POST['Weight'];
-	$status = "";
-	$total_inches = $inches + ($feet * 12);
-	
-	$weight_kg = $weight_lbs * .45;
-	$meters = $total_inches * .025;
-	$meters_squared = pow($meters, 2);
-	$bmi = $weight_kg/$meters_squared;
-	if ($bmi < 18.5)
-	{
-		$status = "Underweight";
-	}
-	else if ($bmi < 24.9)
-	{
-		$status = "Normal";
-	}
-	else if ($bmi < 30.0)
-	{
-		$status = "Normal";
-	}
-	else
-	{
-		$status = "Obese";
-	}
+    $errTrigger = 0;
+
+	if (!isset($feet) && !isset($weight_lbs) && !isset($inches)) {
+	    $errTrigger = 1;
+    }
+    if ($errTrigger == 0){
+        $status = "";
+        $total_inches = $inches + ($feet * 12);
+
+        $weight_kg = $weight_lbs * .45;
+        $meters = $total_inches * .025;
+        $meters_squared = pow($meters, 2);
+        $bmi = $weight_kg/$meters_squared;
+        if ($bmi < 18.5)
+        {
+            $status = "Underweight";
+        }
+        else if ($bmi < 24.9)
+        {
+            $status = "Normal";
+        }
+        else if ($bmi < 30.0)
+        {
+            $status = "Normal";
+        }
+        else
+        {
+            $status = "Obese";
+        }?>
+    <h1>Your bmi is </h1><?php echo $bmi;?><h1>This is considered</h1><?php echo $status;?>
+    <?php
+    }
 	?>
+
+
+    <p style="color: red">
+        <?php
+        if($errTrigger == 1) {
+            echo "Cannot calculate bmi without necessary information. Please go back and put in everything needed";
+        }
+        ?>
+    </p>
 	
-		<h1>Your bmi is </h1><?php echo $bmi;?><h1>This is considered</h1><?php echo $status;?>
+
 
 	</body>
 </html>
--- retirement.php
+++ retirement.php
@@ -14,21 +14,41 @@
 	$salary = $_POST['salary'];
 	$amount_saved = $_POST['companyMatch'];
 	$goal = $_POST['goal'];
-	
-	$match = $amount_saved * .35;
-	$total_amount_one_year = $match + $amount_saved;
-	$total_years = 0;
-	
-	$total_amount = 0;
-	while ($total_amount < $goal)
-	{
-		$total_amount += $total_amount_one_year;
-		$total_years += 1;
-	}
-	$age_at_goal = $age + $total_years;
+
+    $errTrigger = 0;
+
+    if (!isset($feet) && !isset($weight_lbs) && !isset($inches)) {
+        $errTrigger = 1;
+    }
+    if ($errTrigger == 0)
+    {
+
+        $match = $amount_saved * .35;
+        $total_amount_one_year = $match + $amount_saved;
+        $total_years = 0;
+
+        $total_amount = 0;
+        while ($total_amount < $goal)
+        {
+            $total_amount += $total_amount_one_year;
+            $total_years += 1;
+        }
+        $age_at_goal = $age + $total_years;
+        ?>
+        <h1>Your age at desired goal <?php echo $age_at_goal ?>.</h1>
+    <?php
+        echo "";
+    }
 	?>
-	
-	<h1>Your age at desired goal <?php echo $age_at_goal ?>.</h1>
+
+    <p style="color: red">
+        <?php
+        if($errTrigger == 1) {
+            echo "Cannot calculate bmi without necessary information. Please go back and put in everything needed";
+        }
+        ?>
+    </p>
+
 	
 	
 
